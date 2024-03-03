4e192635f80314193b5046ecc9f4356270b52efd
Marcos Brendon De Paula <marcos.brendon@ufms.br>
2024-03-03 18:15:36 -0300

    slime seguindo

--- Assets/Scripts/Mob/EntityMoveAgentAi.cs
+++ Assets/Scripts/Mob/EntityMoveAgentAi.cs
@@ -7,6 +7,8 @@ public class EntityMoveAgentAi : MonoBehaviour
     public Rigidbody2D rb;
     public float MAX_RANGE = 2f;
     public float backwardDistance = 0.5f; // Distância para recuar quando o mob encosta no jogador
+    public float distanceToMaintain = 1.5f; // Distância a ser mantida do jogador
+    public bool isWithinMaintainDistance = false; // Variável para indicar se está dentro da distância de manutenção
 
     private Entity Entity;
     private GameObject targetPlayer;
@@ -29,6 +31,11 @@ public class EntityMoveAgentAi : MonoBehaviour
             if (newTargetPlayer != null && newTargetPlayer != targetPlayer)
             {
                 targetPlayer = newTargetPlayer;
+                isWithinMaintainDistance = false; // Reinicia a variável ao trocar de jogador
+            }
+            else
+            {
+                isWithinMaintainDistance = false; // Define como false se nenhum jogador próximo for encontrado
             }
 
             // Aguarda antes de encontrar o próximo jogador mais próximo
@@ -62,6 +69,17 @@ public class EntityMoveAgentAi : MonoBehaviour
             float distanceToPlayer = Vector2.Distance(transform.position, targetPlayer.transform.position);
             if (distanceToPlayer <= MAX_RANGE)
             {
+                // Verifica se está dentro da distância de manutenção
+                if (distanceToPlayer <= distanceToMaintain)
+                {
+                    isWithinMaintainDistance = true;
+                    return; // Para o método FixedUpdate aqui se estiver dentro da distância de manutenção
+                }
+                else
+                {
+                    isWithinMaintainDistance = false;
+                }
+
                 // Calcula a direção do jogador mais próximo
                 Vector2 direction = (targetPlayer.transform.position - transform.position).normalized;
 
